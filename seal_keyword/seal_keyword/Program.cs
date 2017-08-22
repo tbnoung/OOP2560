7f2d25b7fc7dfc239e3f8e4f1abac7331d00f00d
koson <koson.trachu@gmail.com>
2017-08-23 01:02:37 +0700

    sealed

---
+++ seal_keyword/seal_keyword/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+sealed class SealedClass
+{
+    public int x;
+    public int y;
+}
+
+class SealedTest2
+{
+    static void Main()
+    {
+        SealedClass sc = new SealedClass();
+        sc.x = 110;
+        sc.y = 150;
+        Console.WriteLine("x = {0}, y = {1}", sc.x, sc.y);
+    }
+}
