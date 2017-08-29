beb863850d6f2e6c69ef6c1f172568d04b5c56dc
Tbnoung <Tbnoung@gmail.com>
2017-08-29 19:57:29 +0700

    INSERT ค่าที่ยังไม่มีในของเดิม

--- Week1/CSharpBasic/CSharpBasic/Form1.Designer.cs
+++ Week1/CSharpBasic/CSharpBasic/Form1.Designer.cs
@@ -32,14 +32,14 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textMaximum = new System.Windows.Forms.TextBox();
-            this.textMinimum = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textSize = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textType = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -48,6 +48,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -80,14 +87,14 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.textMaximum);
-            this.groupBox2.Controls.Add(this.textMinimum);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox3);
             this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.textSize);
+            this.groupBox2.Controls.Add(this.textBox2);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.textType);
+            this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Location = new System.Drawing.Point(202, 6);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(312, 132);
@@ -104,19 +111,19 @@
             this.label1.TabIndex = 11;
             this.label1.Text = "Max";
             // 
-            // textMaximum
+            // textBox4
             // 
-            this.textMaximum.Location = new System.Drawing.Point(45, 97);
-            this.textMaximum.Name = "textMaximum";
-            this.textMaximum.Size = new System.Drawing.Size(184, 20);
-            this.textMaximum.TabIndex = 10;
+            this.textBox4.Location = new System.Drawing.Point(45, 97);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(184, 20);
+            this.textBox4.TabIndex = 10;
             // 
-            // textMinimum
+            // textBox3
             // 
-            this.textMinimum.Location = new System.Drawing.Point(45, 71);
-            this.textMinimum.Name = "textMinimum";
-            this.textMinimum.Size = new System.Drawing.Size(184, 20);
-            this.textMinimum.TabIndex = 9;
+            this.textBox3.Location = new System.Drawing.Point(45, 71);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(184, 20);
+            this.textBox3.TabIndex = 9;
             // 
             // label5
             // 
@@ -127,12 +134,12 @@
             this.label5.TabIndex = 7;
             this.label5.Text = "Min";
             // 
-            // textSize
+            // textBox2
             // 
-            this.textSize.Location = new System.Drawing.Point(45, 45);
-            this.textSize.Name = "textSize";
-            this.textSize.Size = new System.Drawing.Size(100, 20);
-            this.textSize.TabIndex = 6;
+            this.textBox2.Location = new System.Drawing.Point(45, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
             // 
             // label3
             // 
@@ -161,15 +168,22 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Size";
             // 
-            // textType
+            // textBox1
             // 
-            this.textType.Location = new System.Drawing.Point(46, 19);
-            this.textType.Name = "textType";
-            this.textType.Size = new System.Drawing.Size(100, 20);
-            this.textType.TabIndex = 3;
+            this.textBox1.Location = new System.Drawing.Point(46, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button13);
+            this.groupBox1.Controls.Add(this.button12);
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
             this.groupBox1.Controls.Add(this.button6);
             this.groupBox1.Controls.Add(this.button5);
             this.groupBox1.Controls.Add(this.button4);
@@ -178,14 +192,14 @@
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(8, 6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(179, 365);
+            this.groupBox1.Size = new System.Drawing.Size(179, 409);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(17, 217);
+            this.button6.Location = new System.Drawing.Point(17, 225);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(106, 23);
             this.button6.TabIndex = 4;
@@ -253,6 +267,76 @@
             this.tabPage2.Text = "If";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(17, 138);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(106, 23);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Short";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(17, 167);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(106, 23);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "UShort";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(17, 196);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(106, 23);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "Int";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(17, 254);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(106, 23);
+            this.button10.TabIndex = 7;
+            this.button10.Text = "Uint";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click_1);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(17, 283);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(106, 23);
+            this.button11.TabIndex = 8;
+            this.button11.Text = "double";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click_1);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(17, 342);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(106, 23);
+            this.button12.TabIndex = 9;
+            this.button12.Text = "decimal";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click_1);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(17, 371);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(106, 23);
+            this.button13.TabIndex = 10;
+            this.button13.Text = "Ulong";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -279,19 +363,26 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textType;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textSize;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.TextBox textMinimum;
+        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textMaximum;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
     }
 }
 
--- Week1/CSharpBasic/CSharpBasic/Form1.cs
+++ Week1/CSharpBasic/CSharpBasic/Form1.cs
@@ -19,44 +19,128 @@ namespace CSharpBasic
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textType.Text = "bool";
-            textSize.Text = sizeof(bool).ToString();
-  
+            textBox1.Text = "bool1111";
+            textBox2.Text = sizeof(bool).ToString();
+            textBox3.Text = ("0").ToString();
+            textBox4.Text = ("1").ToString();
+
+
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textType.Text = "char";
-            textSize.Text = sizeof(char).ToString();
-            textMinimum.Text = (char.MinValue - '\0' ).ToString();
+            textBox1.Text = "Char";
+            textBox2.Text = sizeof(char).ToString();
+            textBox3.Text = (char.MinValue - '\0').ToString();
+            textBox4.Text = (char.MaxValue - '\0').ToString();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textType.Text = "unsigned int 16 bit";
-            textSize.Text = sizeof(UInt16).ToString();
+            textBox1.Text = "unsigned int 16 bit";
+            textBox2.Text = sizeof(UInt16).ToString();
+            textBox3.Text = (UInt16.MinValue - '\0').ToString();
+            textBox4.Text = (UInt16.MaxValue - '\0').ToString();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textType.Text = "Sbyte";
-            textSize.Text = sizeof(SByte).ToString();
+            textBox1.Text = "Sbyte";
+            textBox2.Text = sizeof(SByte).ToString();
+            textBox3.Text = sbyte.MinValue.ToString();
+            textBox4.Text = sbyte.MaxValue.ToString();
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            textType.Text = "Long";
-            textSize.Text = sizeof(long).ToString();
+            textBox1.Text = "Long";
+            textBox2.Text = sizeof(long).ToString();
+            textBox3.Text = long.MinValue.ToString();
+            textBox4.Text = long.MaxValue.ToString();
 
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            textType.Text = "float";
-            textSize.Text = sizeof(float).ToString();
-            textMinimum.Text = float.MinValue.ToString();
-            textMaximum.Text = float.MaxValue.ToString();
+            textBox1.Text = "float";
+            textBox2.Text = sizeof(float).ToString();
+            textBox3.Text = float.MinValue.ToString();
+            textBox4.Text = float.MaxValue.ToString();
 
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Byte";
+            textBox2.Text = sizeof(byte).ToString();
+            textBox3.Text = byte.MinValue.ToString();
+            textBox4.Text = byte.MaxValue.ToString();
+
+        }
+
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Ulong";
+            textBox2.Text = sizeof(ulong).ToString();
+            textBox3.Text = ulong.MinValue.ToString();
+            textBox4.Text = ulong.MaxValue.ToString();
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "Short";
+            textBox2.Text = sizeof(short).ToString();
+            textBox3.Text = short.MinValue.ToString();
+            textBox4.Text = short.MaxValue.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "uShort";
+            textBox2.Text = sizeof(ushort).ToString();
+            textBox3.Text = ushort.MinValue.ToString();
+            textBox4.Text = ushort.MaxValue.ToString();
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "int";
+            textBox2.Text = sizeof(int).ToString();
+            textBox3.Text = int.MinValue.ToString();
+            textBox4.Text = int.MaxValue.ToString();
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "Uint";
+            textBox2.Text = sizeof(uint).ToString();
+            textBox3.Text = uint.MinValue.ToString();
+            textBox4.Text = uint.MaxValue.ToString();
+        }
+
+        private void button11_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "double";
+            textBox2.Text = sizeof(double).ToString();
+            textBox3.Text = double.MinValue.ToString();
+            textBox4.Text = double.MaxValue.ToString();
+        }
+
+        private void button12_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "decimal";
+            textBox2.Text = sizeof(decimal).ToString();
+            textBox3.Text = decimal.MinValue.ToString();
+            textBox4.Text = decimal.MaxValue.ToString();
+        }
+
+        private void button13_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "Ulong";
+            textBox2.Text = sizeof(ulong).ToString();
+            textBox3.Text = ulong.MinValue.ToString();
+            textBox4.Text = ulong.MaxValue.ToString();
         }
     }
 }
